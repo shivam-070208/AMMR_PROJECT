2fdc2e0b2391a4ad4ff54e525e05903f7e01a771
shvgpt-070208 <shvgpt070208@gmail.com>
2025-06-20 16:30:51 +0530

    frontend innitiate

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './App.css'
+const App = () => {
+  return (
+    <div className='w-screen bg-amber-200 h-screen'>
+      
+    </div>
+  )
+}
+
+export default App
